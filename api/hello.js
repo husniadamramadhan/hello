576b18cb284078a497148493cbed83cef0df9025
Husni_Adam <husniadam91@students.unnes.ac.id>
2025-12-17 04:57:22 +0700

    Initial commit

--- api/hello.js
+++ api/hello.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req,res)=>{
-    res.json({message:"Express On Vercel!!!"});
+    res.send("Express On Vercel!!!");
 });
 
 export default app;
